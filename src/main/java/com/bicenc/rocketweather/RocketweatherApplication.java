6cf062453f2a972a225ee29d8a75e6173b7c1e55
Dominik Bicenc <dominik.bicenc@plus4u.net>
2024-10-22 18:40:14 +0200

    Init config

---
+++ src/main/java/com/bicenc/rocketweather/RocketweatherApplication.java
@@ -0,0 +1,13 @@
+package com.bicenc.rocketweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketweatherApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RocketweatherApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bicenc/rocketweather/RocketweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicenc.rocketweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketweatherApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
